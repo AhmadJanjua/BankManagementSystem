9e33889acd95d3dd84dcd8760ec9c52055fccec8
Ahmad <100788678+AhmadJanjua@users.noreply.github.com>
2023-04-10 00:02:07 -0600

    Added Admin features
    Added the department functionality

--- department/urls.py
+++ department/urls.py
@@ -6,4 +6,8 @@ app_name = 'department'
 # directs the urls from root
 urlpatterns = [
     path('create/', create_department, name='create_department'),
+    path('delete/<int:dept_id>/', delete, name='delete'),
+    path('edit/<int:dept_id>/', edit, name='edit'),
+    path('search/', search_dept, name='search'),
+    path('home/', display_dept, name='home'),
 ]
--- department/views.py
+++ department/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import redirect, render
+from django.db.models import Q
+from django.shortcuts import redirect, render, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from .models import Department
 from .forms import DepartmentForm
 from employee.models import Manager
 
@@ -29,6 +31,61 @@ def create_department(request):
         # if the form wasn't filled before, make a new empty form.
         form = DepartmentForm()
     # Display the form using signup html and form
-    return render(request, '../templates/render_form.html', {'form': form, 'title':title, 'header': header, 'button':button})
+    return render(request, '../templates/render_form.html', {'form': form, 'title':title, 'header': header, 'button': button})
 
 
+@login_required
+def display_dept(request):
+    departments = Department.objects.all().order_by('DNO')
+    return render(request, 'dept_home.html', {'departments': departments})
+
+
+def search_dept(request):
+    print('HERE')
+    searched = request.GET.get('results', '')
+    # retrieve all matching posts
+    if searched:
+        # Perform your search logic here, for example:
+        departments = Department.objects.filter(Q(DNO__contains=searched) | Q(name__contains=searched))
+    else:
+        departments = Department.objects.none()
+    return render(request, 'search.html', {'searched': searched, 'departments': departments})
+
+@login_required
+def delete(request, dept_id):
+    dept = get_object_or_404(Department, DNO=dept_id)
+    dept.delete()
+    previous_url = request.META.get('HTTP_REFERER')
+    if previous_url:
+        return redirect(previous_url, )
+    else:
+        # reload the posts page
+        return redirect('department:home')
+
+@login_required
+def edit(request, dept_id):
+    title = 'Update'
+    header = 'Update Department'
+    button = 'Submit'
+    # Retrieve the model instance to be updated
+    dept = get_object_or_404(Department, DNO=dept_id)
+    if request.method == 'POST':
+        # Create a form instance with the submitted data
+        form = DepartmentForm(request.POST, request.FILES, instance=dept)
+
+        if form.is_valid():
+            # Save the updated model instance
+            form.save()
+
+            previous_url = request.META.get('HTTP_REFERER')
+            if previous_url:
+                return redirect(previous_url, )
+            else:
+                # reload the posts page
+                return redirect('department:home')
+    else:
+        # Create a form instance with the data from the model instance to be updated
+        form = DepartmentForm(instance=dept)
+
+    # Render the update form template with the form and model instance
+    return render(request, '../templates/render_form.html', {'form': form, 'title': title, 'header': header, 'button': button})
