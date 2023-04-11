e098d18a6d6e0d5dd5793edd0dc91ae4157178fc
Amrit Mahendrarajah <amritm3000@gmail.com>
2023-04-11 12:11:42 -0600

    Can successfully update loans now

--- loan/urls.py
+++ loan/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import create_loan
+from .views import create_loan, view_modify_loan
 
 # sets the namespace
 app_name = 'loan'
 # directs the urls from root
 urlpatterns = [
     path('create_loan/<str:cid>', create_loan , name='create_loan'),
+    path('view_modify_loan/<int:loanNo>',view_modify_loan,name='view_modify_loan'),
 ]
--- loan/views.py
+++ loan/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from employee.models import Advisor
 from customer.models import Customer
+from .models import Loan
 from .forms import LoanForm
 # Create your views here.
 @login_required
@@ -33,3 +34,28 @@ def create_loan(request,cid):
     else:
         form = LoanForm()
     return render(request,'../templates/render_form.html',{'form':form, 'title': title, 'header':header,'button':button})
+def view_modify_loan(request,loanNo):
+    try:
+        Advisor.objects.get(pk=request.user.id)
+    except:
+        return redirect('home:home')
+    loan = Loan.objects.get(pk=loanNo)
+
+    if request.method == 'POST':
+        # keep a copy of the loan form info
+        form = LoanForm(request.POST)
+        #make sure there are no errors in the form
+        if form.is_valid():
+            #create a loan object without submitting it to the database
+            loan_form = form.save(commit=False)
+            loan_form.remaining = loan_form.amount
+            loan_form.advisor = Advisor.objects.get(pk=request.user.id)
+            loan_form.customer = loan.customer
+
+            loan_form.save()
+            return render(request,'../templates/success.html')
+    else:    
+        form = LoanForm(instance=loan)
+        return render(request,'../templates/render_form.html',{'form':form, 'title': 'Loan','header': 'modify loan', 'button': 'Submit'})
+
+
