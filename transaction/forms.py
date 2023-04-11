1f01e864b073ecd95011fd50380f169ba96e4223
Amrit Mahendrarajah <amritm3000@gmail.com>
2023-04-11 16:47:56 -0600

    Kinda broken pls fix

--- transaction/forms.py
+++ transaction/forms.py
@@ -1,9 +1,21 @@
 from django import forms
 from .models import Transaction
-
+from account.models import Account
+from loan.models import Loan
+from customer.models import Customer
 
 class TransactionForm(forms.ModelForm):
-
+      
     class Meta:
         model = Transaction
         fields = ('amount', 'account', 'loan')
+
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args,**kwargs)
+        customer = kwargs.pop('customer')
+        #customer_accounts = 
+        #customer_loans =
+        if customer:
+            self.fields['account'].queryset = Account.objects.filter(customer=customer)
+            self.fields['loan'].queryset =  Loan.objects.filter(customer=customer)
+    
--- transaction/views.py
+++ transaction/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import TransactionForm
 from employee.models import Teller
 from customer.models import Customer
+from account.models import *
+from loan.models import Loan
 
 def perform_trans(request,cid):
     try:
@@ -16,20 +18,25 @@ def perform_trans(request,cid):
     # check if the request is POST
     if request.method == 'POST':
         # keep a copy of the signup fill info
-        form = TransactionForm(request.POST)
+        form =  TransactionForm(customer=target_customer,data=request.POST)
         # make sure there are no errors
         if form.is_valid():
             # create a user object without submitting it to the database
             tran = form.save(commit=False)
             tran.teller = request.user
             tran.customer = Customer.objects.get(pk=cid)
+
+            #need to list
             # commit the user to the database
             tran.save()
 
             return render(request, '../templates/success.html')
     else:
         # if the form wasn't filled before, make a new empty form.
-        form = TransactionForm()
+        target_customer = get_object_or_404(Customer,ssn=cid)
+        form = TransactionForm(customer=target_customer)
+        #form.fields["account"].queryset = Account.objects.filter(customer=target_customer)
+        #form.fields["loan"].queryset = Loan.objects.filter(customer=target_customer)
     # Display the form using signup html and form
     return render(request, '../templates/render_form.html',
                   {'form': form, 'title': title, 'header': header, 'button': button})
